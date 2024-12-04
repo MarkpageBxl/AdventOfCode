8af95be21a7d552d6d4462d62eaad9336e4b8feb
Markus Lindström <markus@markpage.be>
2024-12-04 08:54:39 +0100

    Generate CSV output for leaderboard.

--- leaderboard/leaderboard.py
+++ leaderboard/leaderboard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import csv
 import json
 from datetime import datetime
 
@@ -13,18 +14,22 @@ args = parser.parse_args()
 with open(args.file) as fp:
     data = json.load(fp)
 
-for id, member in data["members"].items():
-    print(f"#{id}: {member['name']}")
-    for day, dayta in sorted(
-        member["completion_day_level"].items(), key=lambda x: int(x[0])
-    ):
-        for part in ("1", "2"):
-            if part in dayta:
-                print(
-                    f"{int(day):02d}.{part}:",
-                    pytz.timezone("Europe/Brussels").localize(
+with open("leaderboard.csv", "w", newline="") as fp:
+    writer = csv.DictWriter(fp, fieldnames=["id", "name", "day", "part", "ts"])
+    writer.writeheader()
+
+    for id, member in data["members"].items():
+        record = {"id": id, "name": member["name"]}
+        for day, dayta in sorted(
+            member["completion_day_level"].items(), key=lambda x: int(x[0])
+        ):
+            record["day"] = day
+            for part in ("1", "2"):
+                record["part"] = part
+                if part in dayta:
+                    record["ts"] = pytz.timezone("Europe/Brussels").localize(
                         datetime.fromtimestamp(dayta[part]["get_star_ts"])
-                    ),
-                )
-        print("-" * 5)
-    print("=" * 10)
+                    )
+                else:
+                    record["ts"] = None
+                writer.writerow(record)
