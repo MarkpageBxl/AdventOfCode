e9be6b0eaf6499ace9a18c1004c4d74cae5287a9
Markus Lindström <markus@markpage.be>
2024-12-07 10:17:10 +0100

    Add solution for part 2, day 7, 2023.

---
+++ 2023/07/part2.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#
+# Solution for Advent of Code 2023
+# Day 7, part 2
+#
+# Challenge URL: https://adventofcode.com/2023/day/7#part2
+#
+# Author: Markus Lindström <markus@markpage.be>
+#
+
+import itertools
+from collections import defaultdict
+from collections.abc import Iterable
+
+CARD_ORDER = "AKQT98765432J"[::-1]
+
+
+def card_strength(card: str) -> int:
+    pos = CARD_ORDER.find(card)
+    assert pos != -1
+    return pos
+
+
+def generate_joker_hands(hand: str, joker_positions: list[int]) -> Iterable[str]:
+    product = itertools.product(CARD_ORDER[1:], repeat=len(joker_positions))
+    for p in product:
+        mod_hand = list(hand)
+        for i, joker_position in enumerate(joker_positions):
+            mod_hand[joker_position] = p[i]
+        yield "".join(mod_hand)
+
+
+def hand_type_strength_jokers(hand: str) -> int:
+    joker_positions = [i for i, _ in enumerate(hand) if hand[i] == "J"]
+    if len(joker_positions) == 0:
+        return hand_type_strength(hand)
+    best_hand = 0
+    for hand in generate_joker_hands(hand, joker_positions):
+        hand_value = hand_type_strength(hand)
+        if hand_value > best_hand:
+            best_hand = hand_value
+    return best_hand
+
+
+def hand_type_strength(hand: str) -> int:
+    card_counter = defaultdict(int)
+    for card in hand:
+        card_counter[card] += 1
+    # reverse lookup
+    reverse_lookup = defaultdict(set)
+    for card, count in card_counter.items():
+        reverse_lookup[count].add(card)
+    # five of a kind
+    if 5 in reverse_lookup:
+        return 7
+    # four of a kind
+    if 4 in reverse_lookup:
+        return 6
+    # full house
+    if 3 in reverse_lookup and 2 in reverse_lookup:
+        return 5
+    # three of a kind
+    if 3 in reverse_lookup:
+        return 4
+    # two pair
+    if 2 in reverse_lookup and len(reverse_lookup[2]) == 2:
+        return 3
+    # one pair
+    if 2 in reverse_lookup:
+        return 2
+    # high card
+    return 1
+
+
+def hand_sort_key(hand: str):
+    key = [hand_type_strength_jokers(hand)]
+    key += [card_strength(card) for card in hand]
+    return tuple(key)
+
+
+hands = []
+with open("input") as fp:
+    for line in fp:
+        line = line.strip()
+        split = line.split()
+        hand, bid = split[0], int(split[1])
+        hands.append((hand, bid))
+
+hands.sort(key=lambda h: hand_sort_key(h[0]))
+
+winnings = 0
+for i, (hand, bid) in enumerate(hands):
+    rank = i + 1
+    winnings += rank * bid
+
+print(winnings)
